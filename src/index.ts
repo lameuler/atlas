57a639cf328b2303bdf5157e846132ba28ba835a
lam eu ler <27113373+lameuler@users.noreply.github.com>
2025-01-23 22:37:00 +0800

    add heading links

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
 import virtual from '@rollup/plugin-virtual'
 import type { AstroConfig, AstroIntegration } from 'astro'
+import { h } from 'hastscript'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import { ReflectionSymbolId } from 'typedoc'
 
 import { getExports } from './model/model.js'
@@ -114,6 +117,30 @@ export default function atlas(options: AtlasOptions): AstroIntegration {
                     },
                 })
                 globalThis.atlasAstroMarkdownOptions = finalConfig.markdown
+                updateConfig({
+                    markdown: {
+                        rehypePlugins: [
+                            rehypeHeadingIds,
+                            [
+                                rehypeAutolinkHeadings,
+                                {
+                                    behavior: 'append',
+                                    content: h(
+                                        'svg.icon',
+                                        { viewBox: '0 0 24 24', width: 20, height: 20 },
+                                        h('path', { d: 'M9 15l6 -6' }),
+                                        h('path', {
+                                            d: 'M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464',
+                                        }),
+                                        h('path', {
+                                            d: 'M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463',
+                                        }),
+                                    ),
+                                },
+                            ],
+                        ],
+                    },
+                })
             },
             'astro:config:done': (options) => {
                 config = options.config
